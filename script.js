1ee7922ecd6b3c9e6bcb08d8bc6ed8d84f742c92
Luke Cheng <Lukeecheng@gmail.com>
2025-01-31 18:51:27 -0800

    updated updateCounter()

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const counter = document.querySelector('.counter-number');
 async function updateCounter() {
   let response = await fetch("https://2etq6dd5hazdylhnt7z36zznvu0zwdvm.lambda-url.us-east-1.on.aws/");
   let data = await response.json();
-  counter.innerHTML = `Views: ${data}`
+  counter.innerHTML = `Views: ${data.views}`
 }
 
 updateCounter();
