f9575a12984933951d5171567c37e4f297fd8e48
Luke Cheng <Lukeecheng@gmail.com>
2025-01-31 18:42:45 -0800

    included lamda output

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const counter = document.querySelector('.counter-number');
+async function updateCounter() {
+  let response = await fetch("https://2etq6dd5hazdylhnt7z36zznvu0zwdvm.lambda-url.us-east-1.on.aws/");
+  let data = await response.json();
+  counter.innerHTML = `Views: ${data}`
+}
+
+updateCounter();
